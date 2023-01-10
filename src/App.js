ac13f15edab1c9ff926bafa6ab8fdcfe973aad21
musabbir <musabbirrockzz123@gmail.com>
2023-01-11 01:56:57 +0530

    main

--- src/App.js
+++ src/App.js
@@ -1,15 +1,60 @@
+import React, { useState } from "react";
 import "./App.css";
-// import About from "./components/About";
+import Alert from "./components/Alert";
+import About from "./components/About";
 import Navbar from "./components/Navbar";
 import Textform from "./components/Textform";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
 function App() {
+  const [Mode, setMode] = useState("light");
+  const [alert, setAlert] = useState(null);
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type,
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 2000);
+  };
+  const toggleMode = () => {
+    if (Mode === "dark") {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
+      showAlert("Light Mode has Enabled", "success");
+    } else {
+      setMode("dark");
+      document.body.style.backgroundColor = "#042743";
+      showAlert("Dark Mode has Enabled", "success");
+    }
+  };
   return (
     <>
-      <Navbar title="My App" search="Props Change"/>
-      <div className="container my-3">
-      <Textform heading="Enter the Text to Analyze"/>
-      {/* <About></About> */}
-      </div>
+      <Router>
+        <Navbar
+          title="My App"
+          search="Props Change"
+          mode={Mode}
+          toggleMode={toggleMode}
+        />
+        <Alert alert={alert} />
+        <div className="container my-3">
+          <Routes>
+            <Route exact path="/about" element={<About />}></Route>
+            <Route
+              exact path="/"
+              element={
+                <Textform
+                  heading="Enter text to analyze"
+                  mode={Mode}
+                  showAlert={showAlert}
+                />
+              }
+            ></Route>
+          </Routes>
+        </div>
+      </Router>
     </>
   );
 }
--- src/components/Textform.js
+++ src/components/Textform.js
@@ -1,73 +1,121 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 export default function Textform(props) {
-  const handleUpClick= ()=>{
+  const handleUpClick = () => {
     //console.log("UpperCase was Clicked " + Text);
-    let newText=Text.toUpperCase();
+    let newText = Text.toUpperCase();
     setText(newText);
-  }
+    props.showAlert("Converted in Uppercase", "success")
+  };
 
-  const handleLowClick=()=>{
+  const handleLowClick = () => {
     //console.log("LowerCase was Clicked " + Text);
-    let newText=Text.toLocaleLowerCase();
+    let newText = Text.toLocaleLowerCase();
     setText(newText);
-  }
+    props.showAlert("Converted in LowerCase", "success")
+  };
 
-  const handleFindClick=()=>{
+  const handleFindClick = () => {
     //console.log("find was clicked");
-    let newText2=Text.split(Text2).length-1;
+    let newText2 = Text.split(Text2).length - 1;
     setText2(newText2);
-  }
-  const handleClearClick=()=>{
-    let newText="";
-    
+    props.showAlert("Your Text has been found", "success")
+  };
+  const handleClearClick = () => {
+    let newText = "";
+    props.showAlert("Text has been cleared", "success")
+
     setText(newText);
     // setText(newText2);
-  }
+  };
 
-  const HandleOnchange= (event)=>{
+  const handleCopy = () => {
+    var text = document.getElementById("myBox");
+    text.select();
+    navigator.clipboard.writeText(text.value);
+    props.showAlert("Your text has been copied", "success")
+  };
+  const handleExtraSpaces = () => {
+    let newText = Text.split(/[ ]+/);
+    setText(newText.join(" "));
+    props.showAlert("Extra spaces has been removed", "success")
+  };
+
+  const HandleOnchange = (event) => {
     //console.log("Onchange");
     setText(event.target.value);
-  }
-  const HandleOnchange2= (event)=>{
+  };
+  const HandleOnchange2 = (event) => {
     //console.log("Onchange");
     setText2(event.target.value);
-  }
+  };
 
-
-  const [Text,setText]=useState("")
-  const[Text2,setText2]=useState("")
+  const [Text, setText] = useState("");
+  const [Text2, setText2] = useState("");
   return (
     <>
-    <div>
-      <h1>{props.heading}</h1>
-      <div className="mb-3">
-        <textarea
-          className="form-control my-3" onChange={HandleOnchange}
-          id="myBox" value={Text}
-          rows="6"
-        ></textarea>
-        <textarea
-          className="form-control my-3" onChange={HandleOnchange2}
-          id="myBox" value={Text2}
-          rows="2"
-        ></textarea>
+      <div
+        className="container"
+        style={{ color: props.mode === "light" ? "black" : "white" }}
+      >
+        <h1>{props.heading}</h1>
+        <div className="mb-3">
+          <textarea
+            className="form-control my-3"
+            style={{
+              backgroundColor: props.mode === "light" ? "white" : "grey",
+              color: props.mode === "light" ? "black" : "white"
+            }}
+            onChange={HandleOnchange}
+            id="myBox"
+            value={Text}
+            rows="6"
+          ></textarea>
+          <textarea
+            className="form-control my-3"
+            onChange={HandleOnchange2}
+            style={{
+              backgroundColor: props.mode === "light" ? "white" : "grey",
+              color: props.mode === "light" ? "black" : "white"
+            }}
+            id="myBox"
+            value={Text2}
+            rows="2"
+          ></textarea>
+        </div>
+        <button className="btn btn-primary mx-1" onClick={handleUpClick}>
+          Convert to UPPERCASE
+        </button>
+        <button className="btn btn-primary mx-1" onClick={handleLowClick}>
+          Convert to LowerCase
+        </button>
+        <button className="btn btn-primary mx-1" onClick={handleFindClick}>
+          Find{" "}
+        </button>
+        <button className="btn btn-primary mx-1" onClick={handleCopy}>
+          Copy Text
+        </button>
+        <button className="btn btn-primary mx-1" onClick={handleExtraSpaces}>
+          Remove extra spaces
+        </button>
+        <button className="btn btn-primary mx-1" onClick={handleClearClick}>
+          Clear{" "}
+        </button>
       </div>
-      <button className="btn btn-primary mx-3" onClick={handleUpClick}>Convert to UPPERCASE</button>
-      <button className="btn btn-primary mx-3" onClick={handleLowClick}>Convert to LowerCase</button>
-      <button className="btn btn-primary mx-3" onClick={handleFindClick}>Find </button>
-      <button className="btn btn-primary mx-3" onClick={handleClearClick}>Clear </button>
-
-    </div>
-    <div className="container my-3">
-      <h1>Your Text Summary</h1>
-      <p>Your Sentence Has {Text.split(" ").length} Words and {Text.length} Character</p>
-      <p>This takes {0.008 * Text.split(" ").length} Minutes to Read.</p>
-      {/* <p>{Text2} Appeared {Text.split(Text2).length-1} Times</p> */}
-      <h3>Preview</h3>
-      <p>{Text}</p>
-    </div>
-  </>
-
-  )
+      <div
+        className="container my-3"
+        style={{ color: props.mode === "light" ? "black" : "white" }}
+      >
+        <h1>Your Text Summary</h1>
+        <p>
+          Your Sentence Has {Text.split(" ").length} Words and {Text.length}{" "}
+          Character
+        </p>
+        <p>This takes {0.008 * Text.split(" ").length} Minutes to Read.</p>
+        {/* <p>{Text2} Appeared {Text.split(Text2).length-1} Times</p> */}
+        <h3>Preview</h3>
+        <p>{Text.length>0?Text: "Enter Your Text To Preview"}</p>
+      </div>
+    </>
+  );
 }
